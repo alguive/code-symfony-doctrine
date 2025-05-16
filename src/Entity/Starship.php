726b092960b0889ac30b56121345e4a7ad587dd8
alguive <a.guijarro.vera@gmail.com>
2025-05-16 20:33:40 +0200

    Add missing methods

--- src/Entity/Starship.php
+++ src/Entity/Starship.php
@@ -93,4 +93,18 @@ class Starship
 
         return $this;
     }
+
+    public function getStatusString(): string
+    {
+        return $this->status->value;
+    }
+
+    public function getStatusImageFilename(): string
+    {
+        return match ($this->status) {
+            StarshipStatusEnum::WAITING => 'images/status-waiting.png',
+            StarshipStatusEnum::COMPLETED => 'images/status-complete.png',
+            StarshipStatusEnum::IN_PROGRESS => 'images/status-in-progress.png',
+        };
+    }
 }
