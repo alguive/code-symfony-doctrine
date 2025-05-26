acb2f7e602e7d13f4dcf4dcaffd214b59471c39f
alvaro.guijarro <alvaro.guijarro@pronovias.com>
2025-05-26 08:37:25 +0200

    Update Starship controller to work with slug
    Now URL works with the slug instead of the ID

--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -2,22 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Starship;
 use App\Model\StarshipRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Repository\StarshipRepository as Repository;
 
 class StarshipController extends AbstractController
 {
-    #[Route('/starships/{id<\d+>}', name: 'app_starship_show')]
-    public function show(int $id, Repository $repository): Response
-    {
-//        $ship = $entityManager->find(Starship::class, $id);
-        $ship = $repository->find($id);
-        if (!$ship) {
-            throw $this->createNotFoundException('Starship not found');
-        }
+    #[Route('/starships/{slug}', name: 'app_starship_show')]
+    public function show(
+        #[MapEntity(mapping: ['slug' => 'slug'])]
+        Starship $ship,
+    ): Response {
+
 
         return $this->render('starship/show.html.twig', [
             'ship' => $ship,
